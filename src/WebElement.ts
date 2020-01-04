89dc8191ebe66768100f998263c49d9066d4bbc2
Kylee Tilley <kyleetilley@gmail.com>
2020-01-04 02:08:02 -0600

    Add findElement methods to WebElement

--- src/WebElement.ts
+++ src/WebElement.ts
@@ -8,6 +8,30 @@ export default class WebElement {
     public elementId: string
   ) {}
 
+  async findElement(by: By, WebElementClass = WebElement) {
+    return this.browser.findElementFromElement(
+      this.elementId,
+      by,
+      WebElementClass
+    );
+  }
+
+  async $(selector: string, WebElementClass = WebElement) {
+    return this.findElement(new By("css selector", selector), WebElementClass);
+  }
+
+  async findElements(by: By, WebElementClass = WebElement) {
+    return this.browser.findElementsFromElement(
+      this.elementId,
+      by,
+      WebElementClass
+    );
+  }
+
+  async $$(selector: string, WebElementClass = WebElement) {
+    return this.findElements(new By("css selector", selector), WebElementClass);
+  }
+
   async text() {
     return await this.browser.elementText(this);
   }
