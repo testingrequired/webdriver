755ccb996ef7aa7d9b9c7bdb206134066d51b4a0
Kylee Tilley <kyleetilley@gmail.com>
2020-01-03 23:39:11 -0600

    WIP

--- bin/cli.js
+++ bin/cli.js
@@ -5,16 +5,6 @@ const { WebDriver, Browser } = require("../lib");
     remoteUrl: "http://localhost:4444/wd/hub",
     desiredCapabilities: {
       browserName: "chrome"
-      // chromeOptions: {
-      //   args: [
-      //     "binary=/usr/bin/google-chrome-stable",
-      //     "remote-debugging-port=9222",
-      //     "window-size=1920,1080",
-      //     "headless",
-      //     "disable-gpu",
-      //     "disable-features=VizDisplayCompositor"
-      //   ]
-      // }
     }
   });
 
