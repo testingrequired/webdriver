192bc7118e996af7a34cc94fbc849f8b9e5e0bd8
Kylee Tilley <kyleetilley@gmail.com>
2020-01-04 22:17:05 -0600

    Add more exports to index

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { default as WebDriver } from "./WebDriver";
 export { default as Browser } from "./Browser";
 export { default as WebElement } from "./WebElement";
 export { By } from "./By";
+export { default as Capabilities } from "./Capabilities";
+export { ElementNotFoundError, ElementStateError } from "./errors";
