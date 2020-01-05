a81b6a912b0402db681ce24c6a0707304344535f
Kylee Tilley <kyleetilley@gmail.com>
2020-01-04 21:21:18 -0600

    Update cli and readme

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,25 @@
 const assert = require("assert");
 const { Browser, WebElement } = require("../lib");
 
+(async () => {
+  const webdriverOptions = { remoteUrl: "http://localhost:4444/wd/hub" };
+  const timeoutsConfig = { implicit: 5000 };
+
+  await Browser.chrome(webdriverOptions, timeoutsConfig).session(
+    async browser => {
+      await browser.go("https://exampletest.app/user");
+
+      const loginForm = await browser.$("#loginForm", LoginForm);
+
+      await loginForm.fillAndSubmit("testUser", "password");
+
+      const h3 = await browser.$("h3");
+
+      assert.strictEqual(await h3.text(), "Timeline");
+    }
+  );
+})();
+
 class LoginForm extends WebElement {
   get username() {
     return this.$("#username");
@@ -14,27 +33,9 @@ class LoginForm extends WebElement {
     return this.$("#loginButton");
   }
 
-  async fill(username, password) {
+  async fillAndSubmit(username, password) {
     await (await this.username).sendKeys(username);
     await (await this.password).sendKeys(password);
     await (await this.loginButton).click();
   }
 }
-
-(async () => {
-  const remoteUrl = "http://localhost:4444/wd/hub";
-
-  await Browser.chrome({ remoteUrl }, { implicit: 5000 }).session(
-    async browser => {
-      await browser.go("https://exampletest.app/user");
-
-      const loginForm = await browser.$("#loginForm", LoginForm);
-
-      await loginForm.fill("testUser", "password");
-
-      const h3 = await browser.$("h3");
-
-      assert.strictEqual(await h3.text(), "Timeline");
-    }
-  );
-})();
