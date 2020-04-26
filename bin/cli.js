f152ceee5d3d1be573d1007088cd41809c09df66
Kylee Tilley <kyleetilley@gmail.com>
2020-04-26 17:44:41 -0500

    Fix cli with event changes

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,7 @@ import { Browser, WebElement } from "../lib";
 
 (async () => {
   const webdriverOptions = {
-    remoteUrl: "http://localhost:4444/wd/hub"
+    remoteUrl: "http://localhost:4444/wd/hub",
   };
   const timeoutsConfig = { implicit: 5000 };
 
@@ -13,7 +13,7 @@ import { Browser, WebElement } from "../lib";
     webdriverOptions,
     timeoutsConfig,
     registerOutputHandlers
-  ).session(async browser => {
+  ).session(async (browser) => {
     await browser.go("https://exampletest.app/user");
 
     const loginForm = await browser.$("#loginForm", LoginForm);
@@ -27,34 +27,34 @@ import { Browser, WebElement } from "../lib";
 })();
 
 function registerOutputHandlers(driver) {
-  driver.on("sessionStart", capabilities => {
+  driver.on("sessionStart", (capabilities) => {
     console.log(
       `Session initiated with capabilities: ${JSON.stringify(capabilities)}`
     );
   });
 
-  driver.on("sessionStart:success", sessionId => {
+  driver.on("sessionStart:success", (sessionId) => {
     console.log(`Session started. Session Id: ${sessionId}`);
   });
 
-  driver.on("sessionEnd", sessionId => {
+  driver.on("sessionEnd", (sessionId) => {
     console.log(`Session ended. Session Id: ${sessionId}`);
   });
 
-  driver.on("command", (url, method, body) => {
-    console.log(`COMMAND: ${method} ${url}`);
+  driver.on("command", (requestId, { endpoint, method, body }) => {
+    console.log(`COMMAND (${requestId}): ${method} ${endpoint}`);
 
     if (body) {
       console.log(`REQ: ${JSON.stringify(body)}`);
     }
   });
 
-  driver.on("command:success", body => {
-    console.log(`RES: ${JSON.stringify(body)}`);
+  driver.on("command:success", (requestId, command, body) => {
+    console.log(`RES     (${requestId}): ${JSON.stringify(body)}`);
   });
 
-  driver.on("command:fail", error => {
-    console.log(`ERROR: ${error.message}`);
+  driver.on("command:fail", (requestId, command, error) => {
+    console.log(`ERROR  (${requestId}): ${error.message}`);
   });
 
   driver.on("command:end", () => {
